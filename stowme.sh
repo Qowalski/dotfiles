7527e55ebc9b7b1c8ef2432228acfe1db62f970a
Qowalski <dorbore@web.de>
2019-10-28 18:09:57 +0100

    improved ignore files and default behavior

--- stowme.sh
+++ stowme.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Executes GNU's stow command but filters out all files listed in .stowignore.
 # If target (-t, --target) is not set it will get set to ~ (/home/<user>).
@@ -9,32 +9,35 @@ if [[ ! -f /bin/stow ]]; then
     exit 1
 fi
 
-if [[ ${#} -le 0 ]]; then
-    args=$(ls)
-    # $0 * && exit 0
-else
-    args=${@}
+args=${@}
+
+if [[ ${#} -le 0 ]] \
+    || [[ ! "${args[@]}" =~ "-S" && ! "${args[@]}" =~ "--stow" \
+    &&! "${args[@]}" =~ "-D" && ! "${args[@]}" =~ "--delete" \
+    && ! "${args[@]}" =~ "-R" && ! "${args[@]}" =~ "--restow" ]]
+then
+    args="$(ls) ${@}"
 fi
 
 IGNORE=$(cat .stowignore 2> /dev/null)
 HOME=~
 TARGET="-t ${HOME}"
 
-echo ${IGNORE[@]}
-
 for arg in ${args[@]}; do
-    if [[ ${IGNORE[@]} =~ ${arg} ]]; then
-        echo "Ignoring '${arg}'"
-        args=( "${args[@]/$arg}" )
-    fi
+    for ig in ${IGNORE[@]}; do
+        if [[ ${arg} == ${ig} ]]; then
+            echo "Ignoring '${arg}'"
+            args=( "${args[@]/$arg}" )
+        fi
+    done
 
     if [[ ${arg} == "-t" ]] || [[ ${arg} == "--target" ]]; then
-        args=""
+        TARGET=""
     fi
 done
 
-echo -e "\e[1;31mstow ${args[@]} ${TARGET}\e[m"
-printf "execute? [y/n] "
+echo -e "\e[1;31mstow ${args[@]} ${TARGET}\e[m" | tr "\n" " " | sed 's/\  */ /g; s/$/\n/g'
+printf "proceed? [y/n] "
 read user
 
 if [[ ${user} == "y" ]]; then
